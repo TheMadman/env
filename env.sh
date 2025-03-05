2d0d7fd8fb56080898e7976d423041b17d578ee4
Marcus Harrison <marcus@harrisonland.co.uk>
2025-03-05 19:29:57 +0100

    more updates

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ export PATH="$PREFIX"/bin:"$PATH"
 export LD_LIBRARY_PATH="$PREFIX"/lib:"$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$LD_LIBRARY_PATH"
 export CPATH="$PREFIX"/include:"$CPATH"
+export CMAKE_INSTALL_PREFIX="$PREFIX"
 
 export PS1="(env: $(echo "$PREFIX" | sed -E s."$HOME".~.)) $PS1"
 
